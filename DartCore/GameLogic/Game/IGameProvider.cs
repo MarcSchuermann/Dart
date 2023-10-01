a50a8facac1d6792f7cb21e22f1597705dbdddbb
Marc Schuermann <marc.schuermann@hotmail.de>
2023-10-01 21:22:12 +0200

    add IGameProvider

---
+++ DartCore/GameLogic/Game/IGameProvider.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IGameProvider.cs" company="Marc Schürmann">
+//     Copyright (c) Marc Schürmann. All Rights Reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Schuermann.Darts.GameCore.Game
+{
+   /// <summary>The game instance provider.</summary>
+   public interface IGameProvider
+   {
+      #region Public Properties
+
+      /// <summary>Gets the game.</summary>
+      /// <value>The game.</value>
+      IGameProcedure Game { get; }
+
+      #endregion Public Properties
+   }
+}
